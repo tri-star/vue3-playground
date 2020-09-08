a97b9e35d413d39ce19b80c3c3bf8088060bc624
tristar <tristar@urban-theory.net>
2020-09-08 21:54:29 +0900

    ネスト↓オブジェクトの確認

--- src/pages/ObjectPage.vue
+++ src/pages/ObjectPage.vue
@@ -5,6 +5,10 @@
     <input type="text" v-model="user.name">
   </div>
   <div class="row">
+    <p>Department</p>
+    <input type="text" v-model="user.department.name">
+  </div>
+  <div class="row">
     <p>Tags</p>
     <button @click="addTag()">add tag</button>
   </div>
@@ -24,14 +28,25 @@ class User {
   public job: string
   public age: Number
   public tags: String[]
+  public department: Department
   
   constructor() {
     this.name = ''
     this.job = ''
     this.age = 0
     this.tags = []
+    this.department = new Department()
   }
 }
+
+class Department {
+  public name: String
+
+  constructor() {
+    this.name = ''
+  }
+}
+
 
 export default defineComponent({
 
